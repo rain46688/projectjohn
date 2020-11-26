7199831d440b26e445d91c220c696f79ed7eec70
rain46688 <rain46688@gmail.com>
2020-11-26 22:21:28 +0900

    알람 헤더  부분 수정중 cms 1126

--- JohnProject/src/main/java/com/kh/john/alarm/socket/AlarmSocketHandler.java
+++ JohnProject/src/main/java/com/kh/john/alarm/socket/AlarmSocketHandler.java
@@ -36,8 +36,7 @@ public class AlarmSocketHandler extends TextWebSocketHandler {
 		Map<String, Object> map = session.getAttributes();
 		Member m = (Member) map.get("loginMember");
 		users.put(m, session);
-		log.debug("m : " + m);
-		log.info("닉네임 : " + m.getMemNickname());
+		log.info("알람 서버 오픈 접속 닉네임 : " + m.getMemNickname());
 	}
 
 //알람 목록 호버하면 나오는 리스트에 시간 초까지 제대로 보이게 수정해보기
@@ -46,45 +45,44 @@ public class AlarmSocketHandler extends TextWebSocketHandler {
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		// TODO Auto-generated method stub
 		log.debug("알람 서버 handleTextMessage, getPayload :  " + message.getPayload());
-
-		//
 		Map<String, Object> map = session.getAttributes();
 		Member m = (Member) map.get("loginMember");
-		log.debug("유저 : " + m);
+		log.info("알람 서버 메세지 접속 닉네임 : " + m.getMemNickname());
+		//
 		if (message.getPayload().equals("list")) {
-			log.debug("list 가져오기 실행, 닉네임 :  " + m.getMemNickname());
+			log.debug("알람 리스트 뽑아오기");
 			Iterator<Member> lit = users.keySet().iterator();
 			while (lit.hasNext()) {
 				Member key = lit.next();
 				if (m.getUsid() == key.getUsid()) {
-					log.debug(m.getMemNickname() + "인 나 자신한테만 쏘기 : " + key.getMemNickname());
+					log.debug("자기 usid로 도착한 알람 리스트 자신한테만 뿌려주기");
 					sendList(key);
 				}
 			}
 		} else {
 			Object oo = objectMapper.readValue(message.getPayload(), Object.class);
-			log.debug("자료형 뭔지 : " + oo.getClass().getName());
+			log.debug("자료형 확인 : " + oo.getClass().getName());
 
 			if (oo.getClass().getName().equals("java.util.ArrayList")) {
+				log.debug("리스트인 경우");
 				List<Alarm> ll = objectMapper.readValue(message.getPayload(), new TypeReference<List<Alarm>>() {
 				});
-				for (Alarm a : ll) {
-					log.debug("알 : " + a);
-				}
 				service.updateAlarmRead(ll);
 
-			} else {
+			} else if (oo.getClass().getName().equals("java.util.LinkedHashMap")) {
+				log.debug("다른 객체인 경우");
 				Alarm almsg = objectMapper.readValue(message.getPayload(), Alarm.class);
+				log.debug("알람 디비 넣기");
 				log.debug("almsg : " + almsg);
+				// 세션에 접속해있지 않아도 디비에는 넣어야됨!
+				// 사용자가 사이트에없어도 알람은 보내야되니깐.
+				service.insertExpertAlarm(almsg);
 				Iterator<Member> it = users.keySet().iterator();
 				while (it.hasNext()) {
 					Member key = it.next();
-					log.debug("알람 디비 넣기");
-					// 세션에 접속해있지 않아도 디비에는 넣어야됨!
-					// 사용자가 사이트에없어도 알람은 보내야되니깐.
-					service.insertExpertAlarm(almsg);
 					// 세션에 접속해있는 사람한테 리스트 발송
 					if (almsg.getAlarmReceiveMemUsid() == key.getUsid()) {
+						log.debug("알람 전송");
 						sendList(key);
 					}
 				}
@@ -110,12 +108,10 @@ public class AlarmSocketHandler extends TextWebSocketHandler {
 	}
 
 	private void sendList(Member m) {
+		log.debug("sendList 메소드 실행");
 		List<Alarm> list = null;
 		try {
 			list = service.selectAlarmList(m.getUsid());
-			for (Alarm a : list) {
-				log.debug("Date : " + a.getAlarmDate());
-			}
 			users.get(m).sendMessage(new TextMessage(objectMapper.writeValueAsString(list)));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
